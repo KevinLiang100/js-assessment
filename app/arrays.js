e794f1368475290714ca094af6ea14d0897ab2ad
Kevin Liang <kevinliang100@gmail.com>
2016-07-12 18:11:35 -0700

    Implemented remove.

--- app/arrays.js
+++ app/arrays.js
@@ -13,7 +13,13 @@ exports.arraysAnswers = {
   },
 
   remove: function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy: function(arr, item) {
