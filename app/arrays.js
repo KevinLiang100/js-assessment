46575a48fc78f411391927e8801db81eb9f39e82
Kevin Liang <kevinliang100@gmail.com>
2016-07-12 22:38:20 -0700

    Implemented findAllOccurences.

--- app/arrays.js
+++ app/arrays.js
@@ -100,6 +100,12 @@ exports.arraysAnswers = {
   },
 
   findAllOccurrences: function(arr, target) {
-
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        result.push(i);
+      }
+    }
+    return result;
   }
 };
